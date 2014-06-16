a28dc167fcb7f7ce8f8b985577d258525d1e5b92
Nick Herhusky <nick.herhusky@gmail.com>
2014-06-16 13:26:51 -0400

    Added the first character controller movement

--- TronTest/Assets/Scripts/FirstPersonController.cs
+++ TronTest/Assets/Scripts/FirstPersonController.cs
@@ -10,6 +10,14 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		float forwardSpeed = Input.GetAxis ("Vertical");
+
+		Vector3 speed = new Vector3 (0, 0, forwardSpeed);
+
+		CharacterController cc = GetComponent<CharacterController>();
+
+		cc.SimpleMove (speed);
 	
 	}
 }
